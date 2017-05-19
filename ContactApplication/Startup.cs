9b0fe922332329b547057ae02b2c0e035d77d21e
James Buck <jcebuck@gmail.com>
2017-05-19 14:00:14 +0100

    Add project files.

---
+++ ContactApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactApplication.Startup))]
+namespace ContactApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
