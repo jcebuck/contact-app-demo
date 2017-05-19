60218ec7d7251f31f04c3fc4534b2d51f10a2b49
James Buck <jcebuck@gmail.com>
2017-05-19 15:31:06 +0100

    Add AngularJS packages

--- ContactApplication/App_Start/BundleConfig.cs
+++ ContactApplication/App_Start/BundleConfig.cs
@@ -23,7 +23,13 @@ namespace ContactApplication
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js",
+                        "~/Scripts/angular-material.js",
+                        "~/Scripts/app.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/angular-material.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
